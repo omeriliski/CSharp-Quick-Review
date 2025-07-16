a48ce8125395946ac22a4cda07a7a36b73909ec1
omeriliski <omeriliski06@gmail.com>
2025-07-16 11:33:23 +0200

    add multiLayerArchitecture

---
+++ MultiLayerArchitecture/EntitiyLayer/Concrete/Customer.cs
@@ -0,0 +1,13 @@
+﻿
+namespace EntitiyLayer.Concrete
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerCity { get; set; }
+        public List<Order> Orders { get; set; }
+        public bool CustomerStatus { get; set; }
+    }
+}
